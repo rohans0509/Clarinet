27de4762f19f339fb0a0daf5c8ab2f53a1c5eaa0
kshitijalwadhi <kshitijalwadhi@gmail.com>
2021-11-22 20:18:18 +0530

    query dump

--- test.py
+++ test.py
@@ -5,15 +5,10 @@ import os
 from tqdm import tqdm
 from Clarinet.preprocessing.clipper import condition, clip
 
-time_for_doc = 20
-offset_for_doc = 0
-time_for_query = 5
-offset_for_query = np.random.randint(1, 14)
-
 
 path = 'Data/Midi/2018/'
 
 # loop through all files in folder Data/Midi/2018
 for file in tqdm(os.listdir('Data/Midi/2018')):
-    clipped = clip(path + file, 'doc')
-    clipped.dump('Data/Midi/2018_clipped/' + file)
+    clipped = clip(path + file, 'query')
+    clipped.dump('Data/Midi/2018_queries/' + file.split('.')[0] + '_query.mid')
