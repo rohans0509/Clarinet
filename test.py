e0b37665b9971c4c54a14c5a8dfc259896c3ebd5
kshitijalwadhi <kshitijalwadhi@gmail.com>
2021-11-22 20:09:58 +0530

    update test.py

--- test.py
+++ test.py
@@ -1,39 +1,19 @@
-import json
-import miditoolkit
+from miditoolkit.midi.containers import Instrument
+from miditoolkit.midi.parser import MidiFile
+import numpy as np
 import os
 from tqdm import tqdm
+from Clarinet.preprocessing.clipper import condition, clip
+
+time_for_doc = 20
+offset_for_doc = 0
+time_for_query = 5
+offset_for_query = np.random.randint(1, 14)
 
 
-def extractNotes(path):
+path = 'Data/Midi/2018/'
 
-    files = os.listdir(path)
-
-    fname_to_notes = {}
-
-    print("Extracting notes...")
-    for fname in tqdm(files):
-        mid_in = miditoolkit.midi.parser.MidiFile(os.path.join(path, fname))
-        notes = mid_in.instruments[0].notes
-        notes = sorted(notes, key=lambda x: x.start)
-        lis = []  # (pitch,start,end,velocity)
-        for e in notes:
-            lis.append([e.pitch, e.start, e.end, e.velocity])
-        fname_to_notes[fname] = lis
-
-    folder_name = path.split('/')[-1]
-
-    output_dir = "Data/Json"
-
-    output_dir = f"{output_dir}/{folder_name}"
-
-    if not os.path.exists(output_dir):
-        os.mkdir(output_dir)
-
-    filename = f"{output_dir}/melody_notes.json"
-
-    with open(filename, 'w') as f:
-        json.dump(fname_to_notes, f)
-
-
-extractNotes("Data/Melody/2018_processed")
-
+# loop through all files in folder Data/Midi/2018
+for file in tqdm(os.listdir('Data/Midi/2018')):
+    clipped = clip(path + file, 'doc')
+    clipped.dump('Data/Midi/2018_clipped/' + file)
