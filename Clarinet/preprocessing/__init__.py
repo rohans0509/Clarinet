ce021368d91545e51c68f92d908f044a39f0d083
rohans0509 <59282751+rohans0509@users.noreply.github.com>
2021-11-23 19:48:42 +0530

    Add getkey

--- Clarinet/preprocessing/__init__.py
+++ Clarinet/preprocessing/__init__.py
@@ -1,5 +1,6 @@
-from .transpose import transpose
+from .transpose import transpose,getKey
 
+from .getBPM import getBPM
 def preprocess(midi_filename):
     print(f"Preprocessing {midi_filename}")
     return(transpose(midi_filename))
