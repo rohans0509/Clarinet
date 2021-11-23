20c93f416dfae219f5576b1ae3a2b2d72e9b7f3e
kshitijalwadhi <kshitijalwadhi@gmail.com>
2021-11-23 20:21:56 +0530

    eval midway

--- Clarinet/search/similarity_time.py
+++ Clarinet/search/similarity_time.py
@@ -1,3 +1,6 @@
+factor = 768
+
+
 def edit_distance(s1, s2):
     m = len(s1)
     n = len(s2)
@@ -43,3 +46,55 @@ def midiEt_to_note_sequence(midiEt_sequence):
         num = midiEt % 12
         note_sequence.append(midiEt_to_note[num + 12])
     return "".join(note_sequence)
+
+
+def splitNotes(notes):
+    new_notes = []
+    for note in notes:
+        start = note[1]/factor
+        end = note[2]/factor
+        startSec = int(start)
+        endSec = int(end)
+
+        if startSec == endSec:
+            new_notes.append(note)
+        else:
+            new_notes.append([note[0], note[1], (startSec+1)*factor, note[3]])
+            for i in range(startSec+1, endSec-1):
+                new_notes.append([note[0], int(i*factor), int(i+1)*factor, note[3]])
+            new_notes.append([note[0], int(endSec*factor), note[2], note[3]])
+    return new_notes
+
+
+def getStrides(notes, size):
+    notes = sorted(notes, key=lambda x: x[1])
+    chunks = []
+    for _ in range(len(notes)+factor):
+        chunks.append([])
+
+    curT = 0
+    nextidx = 0
+    flag = False
+
+    i = 0
+    while(i < len(notes)):
+        st = notes[i][1]
+        maxT = curT + size
+        if st >= maxT:
+            curT += factor
+            i = nextidx
+            flag = False
+            continue
+
+        if st % factor == 0 and flag == False and st != curT:
+            nextidx = i
+            flag = True
+
+        pos = int(curT/factor)
+        chunks[pos].append(notes[i])
+        i += 1
+    ans = []
+    for c in chunks:
+        if len(c) > 0:
+            ans.append(c)
+    return ans
