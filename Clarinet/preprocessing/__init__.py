c11f700df14ede03e0e183decf341f857a39a6cf
rohans0509 <59282751+rohans0509@users.noreply.github.com>
2021-11-23 23:26:43 +0530

    restructure

--- Clarinet/preprocessing/__init__.py
+++ Clarinet/preprocessing/__init__.py
@@ -1,6 +1,5 @@
 from .transpose import transpose,getKey
 
-from .getBPM import getBPM
 def preprocess(midi_filename):
     print(f"Preprocessing {midi_filename}")
     return(transpose(midi_filename))
