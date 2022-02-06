6f074138c20429111d07fe9e55baa6ffd203c2cf
rohans0509 <59282751+rohans0509@users.noreply.github.com>
2022-02-06 16:34:09 +0530

    Add results

--- Clarinet/evaluation/trends.py
+++ Clarinet/evaluation/trends.py
@@ -3,22 +3,27 @@ import pandas as pd
 import os
 
 def trends(file,x):
-    plt.figure()
-    output_dir="/".join(file.split("/")[:-2])+"/Plots"
+    try:
+        plt.figure()
+        output_dir="/".join(file.split("/")[:-2])+"/Plots"
 
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
-    y=f"{file.split('/')[-1].split('.')[0]}"
+        y=f"{file.split('/')[-1].split('.')[0]}"
 
 
-    df=pd.read_csv(file)
+        df=pd.read_csv(file)
+        # sort df by x
+        df=df.sort_values(by=x)
 
-    plt.plot(df[x],df[y],color='red')
-    plt.xlabel(x)
-    plt.ylabel(y)
-    
-    plt.title(f"{y} vs {x}",fontsize=12,fontweight='bold')
-    plt.xticks(range(int(min(df[x])-1),int(max(df[x]))+2))
+        plt.plot(df[x],df[y],color="red")
+        plt.xlabel(x)
+        plt.ylabel(y)
+        
+        plt.title(f"{y} vs {x}",fontsize=12,fontweight='bold')
+        plt.xticks(range(int(min(df[x])-1),int(max(df[x]))+2))
 
-    plt.savefig(f"{output_dir}/{y} vs {x}.png")
+        plt.savefig(f"{output_dir}/{y} vs {x}.png")
+    except:
+        print(f"{file} failed")
